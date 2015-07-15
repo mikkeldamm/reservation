43f73c89787ac1a7c65d91e95c42d4a0e92deabe
mikkeldamm <mikkeldamm@hotmail.com>
2015-07-16 01:19:55 +0200

    Changed some code in Event store

--- src/Seabook.Application/Store/EventStore.cs
+++ src/Seabook.Application/Store/EventStore.cs
@@ -14,26 +14,12 @@ namespace Seabook.Application.Store
 	public class EventStore : IEventStore
 	{
 		private readonly IPublishEvents _eventPublisher;
+		private readonly Dictionary<string, List<DomainEvent>> _inMemoryEventStore = new Dictionary<string, List<DomainEvent>>();
 
 		public EventStore(IPublishEvents eventPublisher)
 		{
 			_eventPublisher = eventPublisher;
 		}
-
-		// Only public to demonstrate events being saved
-		private readonly Dictionary<string, List<DomainEvent>> _inMemoryEventStore = new Dictionary<string, List<DomainEvent>>();
-//		private readonly Dictionary<string, List<DomainEvent>> _inMemoryEventStore = new Dictionary<string, List<DomainEvent>>
-//		{
-//			{
-//				"reservationId1234",
-//				new List<DomainEvent>
-//				{
-//					new ReservationCreated { Id = "reservationId1234" },
-//					new ReservationOutboundDateTimeChanged { DateTime = DateTime.Now.AddDays(1) },
-//					new ReservationOutboundDateTimeChanged { DateTime = DateTime.Now.AddDays(2) },
-//				}
-//			}
-//		};
 
 		public void Save(string aggregateId, IEnumerable<DomainEvent> domainEvents)
 		{
@@ -45,32 +31,36 @@ namespace Seabook.Application.Store
 				_inMemoryEventStore.Add(aggregateId, eventDescriptors);
 			}
 
-			foreach (var domainEvent in domainEvents)
-			{
-				eventDescriptors.Add(domainEvent);
-
-				_eventPublisher.Publish(domainEvent);
-			}
+			eventDescriptors.AddRange(domainEvents);
+			
+			PublishDomainEvents(domainEvents);
 		}
+		
+		public List<DomainEvent> Load(string aggregateId)
+		{
+			if (!Exists(aggregateId))
+			{
+				throw new ArgumentException($"Could not find aggregate event by id [{aggregateId}]");
+			}
 
+			var domainEvents = _inMemoryEventStore[aggregateId];
+			
+			PublishDomainEvents(domainEvents);
+
+			return domainEvents;
+		}
+		
 		public bool Exists(string aggregateId)
 		{
 			return _inMemoryEventStore.ContainsKey(aggregateId);
 		}
 		
-		public List<DomainEvent> Load(string aggregateId)
+		private void PublishDomainEvents(IEnumerable<DomainEvent> domainEvents) 
 		{
-			if (!_inMemoryEventStore.ContainsKey(aggregateId))
-				throw new ArgumentException($"Could not find aggregate event by id [{aggregateId}]");
-
-			var domainEvents = _inMemoryEventStore[aggregateId];
-			
 			foreach (var domainEvent in domainEvents)
 			{
 				_eventPublisher.Publish(domainEvent);
 			}
-
-			return domainEvents;
 		}
 	}
 }
